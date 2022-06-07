f068e6b35b8e294bf4b1eb5e4f7bbfdc42a93b60
arunkb2795 <arunkb27@gmail.com>
2022-06-07 20:46:01 +0530

    useRducer

--- App.tsx
+++ App.tsx
@@ -1,31 +1,42 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useReducer } from 'react';
 
-type AuthUser = {
-  name: string;
-  email: string;
+type counterState = {
+  count: number;
+};
+
+type counterAction = {
+  type: 'INCREMENT' | 'DECREMENT' | 'RESET';
+  payload?: number;
+};
+const initialState = { count: 0 };
+
+const reducer = (state: counterState, action: counterAction) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.payload };
+    case 'DECREMENT':
+      return { count: state.count - action.payload };
+    case 'RESET':
+      return initialState;
+    default:
+      return state;
+  }
 };
 
 export default function App() {
-  const [user, setUser] = useState<AuthUser | null>(null);
-
-  const handleLogin = () => {
-    setUser({
-      name: 'Arun',
-      email: 'arunkb27@gmail.com',
-    });
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <p>User name is {user?.name} </p>
-      <p>User email is {user?.email} </p>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+      count is {state.count}
+      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
+        Increment by 10
+      </button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 10 })}>
+        Increment by 10
+      </button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
     </div>
   );
 }
